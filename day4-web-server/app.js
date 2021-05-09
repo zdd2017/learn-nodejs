9d0aefdd0f0e2459d63ebf0d0795e1175c150e4a
zhengdandan <286814103@qq.com>
2021-05-09 18:39:35 +0800

    处理get post 请求

--- day4-web-server/app.js
+++ day4-web-server/app.js
@@ -2,6 +2,7 @@ const http = require('http')
 // const fs = require('fs')
 const path = require('path')
 const routes = require('./module/routes')
+const ejs = require('ejs')
 
 const server = http.createServer((req, res) => {
     // 如果是访问静态资源，指向static目录下的文件
@@ -10,18 +11,44 @@ const server = http.createServer((req, res) => {
     // 根据不同路径执行不同操作
     const url = new URL(req.url, 'http://127.0.0.1:3000')
     let pathname = url.pathname
-    if(pathname === '/login') {
-        console.log("login")
+    console.log(req.method)
+    // get 请求
+    if(pathname === '/news') {
+        // 获取参数 url: localhost:3000/news?name=rose&age=18
+        let name = url.searchParams.get('name');
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html;charset=utf8')
-        res.end('登录')
+            res.setHeader('Content-Type', 'text/html;charset=utf8')
+            res.end(name)
     }
-    if(pathname === '/register') {
+    else if(pathname === '/login') {
+        // let msg = '从数据库获取的数据';
+        // ejs.renderFile('./views/login.ejs', {msg: msg}, (err, data) => {
+        //     res.statusCode = 200;
+        //     res.setHeader('Content-Type', 'text/html;charset=utf8')
+        //     res.end(data)
+        // }) 
+        ejs.renderFile('./views/form.ejs', {}, (err, data) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html;charset=utf8')
+            res.end(data)
+        })
+    }
+    else if(pathname === '/doLogin') {
+        // 获取post提交的数据
+        let postData = '';
+        req.on('data', (chunk) => {
+            postData+=chunk;
+        })
+        req.on('end', () => {
+            res.end(postData)
+        })
+    }
+    else if(pathname === '/register') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html;charset=utf8')
         res.end('注册')
     }
-    if(pathname === '/admin') {
+    else if(pathname === '/admin') {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html;charset=utf8')
         res.end('用户')
